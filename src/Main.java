bccf1602c865fdd1e25985157dc5ba6f858620e3
Mahdi <amiralmasi021@gmail.com>
2025-09-13 20:19:23 +0330

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Alisun");
+        System.out.println("apple");
+        System.out.println("myfen");
+        System.out.println("myfan");
+        System.out.println("lunchbox");
+        System.out.println("notebook");
+      }
+}
